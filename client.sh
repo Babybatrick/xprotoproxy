9d79314300a12dab323e33d0b531c0fa5543e95f
babybatrick <lezamin@icloud.com>
2025-04-15 02:20:03 +0300

    Changes to be committed: 	modified:   .gitignore 	modified:   client.sh 	modified:   compose.yaml 	modified:   config.sh 	deleted:    proxy/brook/client 	deleted:    proxy/brook/config.json 	deleted:    proxy/nginx/nginx.conf 	deleted:    proxy/snell/client 	deleted:    proxy/snell/config.ini 	deleted:    proxy/socks5/client 	deleted:    proxy/socks5/config.json 	modified:   proxy/trojan/config.json

--- client.sh
+++ client.sh
@@ -7,30 +7,18 @@ cat > ./proxy/hysteria2/client <<EOL
 hysteria2://$PASSWORD@0xhy.$DOMAIN:443?sni=0xhy.$DOMAIN&obfs=salamander&obfs-password=$OBFPASSWORD
 EOL
 
-cat > ./proxy/shadowsocks/client <<EOL
-ss://$(echo -n chacha20-ietf-poly1305:$PASSWORD | base64 -w 0)@0xss.$DOMAIN:443?&tfo=1
+cat > ./proxy/tuic/client <<EOL
+tuic://$UUID:$PASSWORD@0xtc.$DOMAIN:433?sni=0xtc.$DOMAIN&congestion_control=bbr&alpn=h3&upd_relay_mode=native
 EOL
 
-cat > ./proxy/juicity/client <<EOL
-juicity://$UUID:$PASSWORD@0xjc.$DOMAIN:443?sni=0xjc.$DOMAIN&congestion_control=bbr
+cat > ./proxy/shadowsocks/client <<EOL
+ss://$(echo -n chacha20-ietf-poly1305:$PASSWORD | base64 -w 0)@0xss.$DOMAIN:453?&tfo=1
 EOL
 
 cat > ./proxy/trojan/client <<EOL
-trojan://$PASSWORD@0xtj.$DOMAIN:$port_trojan?peer=0xtj.$DOMAIN&alpn=h2
+trojan://$PASSWORD@0xtj.$DOMAIN:463?peer=0xtj.$DOMAIN&alpn=h2
 EOL
 
-cat > ./proxy/brook/client <<EOL
-brook://wssserver?wssserver=wss://0xbk.$DOMAIN:443?/ws&param=%7B%22Host%22:%220x.bk$DOMAIN%22%7D&password=$PASSWORD
-EOL
-
-cat > ./proxy/socks5/client <<EOL
-socks://$(echo -n $UUID:$PASSWORD@0xs5.$DOMAIN:443 | base64 -w 0)
-EOL
-
-cat > ./proxy/snell/client <<EOL
-snell://$(echo -n "chacha20-ietf-poly1305:$PASSWORD" | base64 -w 0)@0xsn.$DOMAIN:443?plugin=obfs-local;obfs=tls;obfs-host=%7B%22Host%22:%22www.baidu.com%22%7D;obfs-uri=/&tfo=1
-EOL
-
-cat > ./proxy/tuic/client <<EOL
-tuic://$UUID:$PASSWORD@0xtc.$DOMAIN:443?sni=0xtc.$DOMAIN&congestion_control=bbr&alpn=h3&upd_relay_mode=native
+cat > ./proxy/juicity/client <<EOL
+juicity://$UUID:$PASSWORD@0xjc.$DOMAIN:473?sni=0xjc.$DOMAIN&congestion_control=bbr
 EOL
--- config.sh
+++ config.sh
@@ -45,101 +45,6 @@ cat > ./proxy/hysteria2/config.json <<EOF
 }
 EOF
 
-cat > ./proxy/shadowsocks/config.json <<EOF
-{ 
-  "server":"0.0.0.0", 
-  "server_port":443, 
-  "local_port":1080, 
-  "password":"$PASSWORD", 
-  "method":"chacha20-ietf-poly1305" 
-}
-EOF
-
-cat > ./proxy/juicity/config.json <<EOF
-{
-  "listen": ":443",
-  "users": {
-    "$UUID": "$PASSWORD"
-  },
-  "certificate": "$CERTPATH",
-  "private_key": "$PKEYPATH",
-  "congestion_control": "bbr",
-  "log_level": "info"
-}
-EOF
-
-cat > ./proxy/trojan/config.json <<EOF
-{
-    "run_type": "server",
-    "local_addr": "0.0.0.0",
-    "local_port": 443,
-    "\$remote_addr": "127.0.0.1",
-    "remote_port": 80,
-    "password": "$PASSWORD",
-    "log_level": 1,
-    "ssl": {
-        "cert": "$CERTPATH",
-        "key": "$PKEYPATH",
-        "key_password": "",
-        "cipher": "ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384",
-        "cipher_tls13": "TLS_AES_128_GCM_SHA256:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_256_GCM_SHA384",
-        "prefer_server_cipher": true,
-        "alpn": [
-            "http/1.1"
-        ],
-        "alpn_port_override": {
-            "h2": 81
-        },
-        "reuse_session": true,
-        "session_ticket": false,
-        "session_timeout": 600,
-        "plain_http_response": "",
-        "curves": "",
-        "dhparam": ""
-    },
-    "tcp": {
-        "prefer_ipv4": false,
-        "no_delay": true,
-        "keep_alive": true,
-        "reuse_port": true,
-        "fast_open": false,
-        "fast_open_qlen": 20
-    }
-}
-EOF
-
-cat > ./proxy/brook/config.json <<EOF
-{
-  "listen": ":443",
-  "password":"$PASSWORD",
-  "domainaddress": "$DOMAIN",
-  "cert": "$CERTPATH",
-  "certkey": "$PKEYPATH",
-  "udpTimeout": "120"
-}
-EOF
-
-cat > ./proxy/socks5/config.json <<EOF
-{
-  "listen": ":443",
-  "users": {"$UUID": "$PASSWORD"},
-}
-EOF
-
-cat > ./proxy/snell/config.ini <<EOF
-[snell-server]
-listen = 0.0.0.0:443
-psk = $PASSWORD
-obfs = tls
-obfs-host = www.baidu.com
-ipv6 = true
-reuse-port = true
-udp = true
-udp-max-size = 4096
-timeout = 600
-dns = 1.1.1.1,1.0.0.1
-EOF
-
 cat > ./proxy/tuic/config.json <<EOF
 {
     "server": "[::]:443",
@@ -164,152 +69,63 @@ cat > ./proxy/tuic/config.json <<EOF
 }
 EOF
 
-cat > ./proxy/nginx/nginx.conf <<EOF
-events {
-    worker_connections 1024;
+cat > ./proxy/shadowsocks/config.json <<EOF
+{ 
+  "server":"0.0.0.0", 
+  "server_port":443, 
+  "local_port":1080, 
+  "password":"$PASSWORD", 
+  "method":"chacha20-ietf-poly1305" 
 }
+EOF
 
-http {
-    server {
-        listen 443;
-        server_name 0xhy.$DOMAIN;
-        # Reverse proxy for Hysteria2
-        location / {
-            proxy_pass http://hysteria2:443;
-            proxy_set_header Host example.com;
-            proxy_set_header X-Real-IP \$remote_addr;
-            proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto \$scheme;
-        }
+cat > ./proxy/trojan/config.json <<EOF
+{
+    "run_type": "server",
+    "local_addr": "0.0.0.0",
+    "local_port": 443,
+    "remote_addr": "127.0.0.1",
+    "remote_port": 80,
+    "password": ["$PASSWORD"],
+    "log_level": 1,
+    "ssl": {
+        "cert": "$CERTPATH",
+        "key": "$PKEYPATH",
+        "key_password": "",
+        "cipher": "ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384",
+        "cipher_tls13": "TLS_AES_128_GCM_SHA256:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_256_GCM_SHA384",
+        "prefer_server_cipher": true,
+        "alpn": [
+            "h2",
+            "http/1.1"
+        ],
+        "reuse_session": true,
+        "session_ticket": false,
+        "session_timeout": 600,
+        "plain_http_response": "",
+        "curves": "",
+        "dhparam": ""
+    },
+    "tcp": {
+        "prefer_ipv4": false,
+        "no_delay": true,
+        "keep_alive": true,
+        "reuse_port": true,
+        "fast_open": false,
+        "fast_open_qlen": 20
     }
+}
+EOF
 
-    server {
-        listen 443;
-        server_name 0xss.$DOMAIN;
-
-        ssl_certificate $CERTPATH;
-        ssl_certificate_key $PKEYPATH;
-
-        # Reverse proxy for Shadowsocks
-        location / {
-            proxy_pass http://shadowsocks:443;
-            proxy_set_header Host example.com;
-            proxy_set_header X-Real-IP \$remote_addr;
-            proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto \$scheme;
-        }
-    }
-
-    server {
-        listen 443;
-        server_name 0xjc.$DOMAIN;
-
-        ssl_certificate $CERTPATH;
-        ssl_certificate_key $PKEYPATH;
-
-        # Reverse proxy for Juicity
-        location / {
-            proxy_pass http://juicity:443;
-            proxy_set_header Host example.com;
-            proxy_set_header X-Real-IP \$remote_addr;
-            proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto \$scheme;
-        }
-    }
-
-    server {
-        listen 443;
-        server_name 0xtj.$DOMAIN;
-
-        ssl_certificate $CERTPATH;
-        ssl_certificate_key $PKEYPATH;
-        
-        # Reverse proxy for Trojan
-        location / {
-            proxy_pass http://trojan:443;
-            proxy_set_header Host example.com;
-            proxy_set_header X-Real-IP \$remote_addr;
-            proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto \$scheme;
-        }
-    }
-
-    server {
-        listen 443;
-        server_name 0xbk.$DOMAIN;
-
-        ssl_certificate $CERTPATH;
-        ssl_certificate_key $PKEYPATH;
-        
-        # Reverse proxy for Brook
-        location / {
-            proxy_pass http://brook:443;
-            proxy_set_header Host example.com;
-            proxy_set_header X-Real-IP \$remote_addr;
-            proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto \$scheme;
-        }
-    }
-
-    server {
-        listen 443;
-        server_name 0xs5.$DOMAIN;
-
-        ssl_certificate $CERTPATH;
-        ssl_certificate_key $PKEYPATH;
-        
-        # Reverse proxy for Socks5
-        location / {
-            proxy_pass http://socks5:443;
-            proxy_set_header Host example.com;
-            proxy_set_header X-Real-IP \$remote_addr;
-            proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto \$scheme;
-        }
-    }
-
-    server {
-        listen 443;
-        server_name 0xsl.$DOMAIN;
-
-        ssl_certificate $CERTPATH;
-        ssl_certificate_key $PKEYPATH;
-        
-        # Reverse proxy for Snell
-        location / {
-            proxy_pass http://snell:443;
-            proxy_set_header Host example.com;
-            proxy_set_header X-Real-IP \$remote_addr;
-            proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto \$scheme;
-        }
-    }
-
-    server {
-        listen 443;
-        server_name 0xtc.$DOMAIN;
-
-        ssl_certificate $CERTPATH;
-        ssl_certificate_key $PKEYPATH;
-        
-        # Reverse proxy for TUIC
-        location / {
-            proxy_pass http://tuic:443;
-            proxy_set_header Host example.com;
-            proxy_set_header X-Real-IP \$remote_addr;
-            proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto \$scheme;
-        }
-    }
-
-    # Fallback for unmatched requests
-    server {
-        listen 443 default_server;
-        server_name _;
-
-        location / {
-            return 404;
-        }
-    }
+cat > ./proxy/juicity/config.json <<EOF
+{
+  "listen": ":443",
+  "users": {
+    "$UUID": "$PASSWORD"
+  },
+  "certificate": "$CERTPATH",
+  "private_key": "$PKEYPATH",
+  "congestion_control": "bbr",
+  "log_level": "info"
 }
 EOF
