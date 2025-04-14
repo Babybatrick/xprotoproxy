889b1bfa8d3c272c059590e73a1455527ce247b4
babybatrick <lezamin@icloud.com>
2025-04-15 02:46:03 +0300

    Changes to be committed: 	modified:   client.sh 	modified:   compose.yaml 	modified:   generator.sh

--- client.sh
+++ client.sh
@@ -4,21 +4,21 @@ source .env
 
 #Creating client URI
 cat > ./proxy/hysteria2/client <<EOL
-hysteria2://$PASSWORD@0xhy.$DOMAIN:443?sni=0xhy.$DOMAIN&obfs=salamander&obfs-password=$OBFPASSWORD
+hysteria2://$PASSWORD@$DOMAIN:443?sni=0xhy.$DOMAIN&obfs=salamander&obfs-password=$OBFPASSWORD
 EOL
 
 cat > ./proxy/tuic/client <<EOL
-tuic://$UUID:$PASSWORD@0xtc.$DOMAIN:433?sni=0xtc.$DOMAIN&congestion_control=bbr&alpn=h3&upd_relay_mode=native
+tuic://$UUID:$PASSWORD@$DOMAIN:433?sni=0xtc.$DOMAIN&congestion_control=bbr&alpn=h3&upd_relay_mode=native
 EOL
 
 cat > ./proxy/shadowsocks/client <<EOL
-ss://$(echo -n chacha20-ietf-poly1305:$PASSWORD | base64 -w 0)@0xss.$DOMAIN:453?&tfo=1
+ss://$(echo -n chacha20-ietf-poly1305:$PASSWORD | base64 -w 0)@$DOMAIN:453?&tfo=1
 EOL
 
 cat > ./proxy/trojan/client <<EOL
-trojan://$PASSWORD@0xtj.$DOMAIN:463?peer=0xtj.$DOMAIN&alpn=h2
+trojan://$PASSWORD@0xtj.$DOMAIN:463?peer=$DOMAIN&alpn=h2
 EOL
 
 cat > ./proxy/juicity/client <<EOL
-juicity://$UUID:$PASSWORD@0xjc.$DOMAIN:473?sni=0xjc.$DOMAIN&congestion_control=bbr
+juicity://$UUID:$PASSWORD@0xjc.$DOMAIN:473?sni=$DOMAIN&congestion_control=bbr
 EOL
--- generator.sh
+++ generator.sh
@@ -55,3 +55,38 @@ EOF
 
 docker compose up -d
 echo "XProto is ready to go!"
+
+echo ""
+echo ""
+echo ""
+echo "HYSTERIA2-----------HYSTERIA2"
+qrencode -t UTF8 $(cat ../proxy/hysteria/client)
+echo $(cat ./proxy/hysteria/client)
+echo ""
+echo ""
+echo ""
+echo "TUIC---------------------TUIC"
+qrencode -t UTF8 $(cat ./proxy/tuic/client)
+echo $(cat ./proxy/tuic/client)
+echo ""
+echo ""
+echo ""
+echo "SHADOWSOCKS-------SHADOWSOCKS"
+qrencode -t UTF8 $(cat ./proxy/shadowsocks/client)
+echo $(cat ./proxy/shadowsocks/client)
+echo ""
+echo ""
+echo ""
+echo "TROJAN-----------------TROJAN"
+qrencode -t UTF8 $(cat ./proxy/trojan/client)
+echo $(cat ./proxy/trojan/client)
+echo ""
+echo ""
+echo ""
+echo "JUICITY---------------JUICITY"
+qrencode -t UTF8 $(cat ./proxy/juicity/client)
+echo $(cat ./proxy/juicity/client)
+echo ""
+echo ""
+echo ""
+echo "Server setup has been completed, configuration URIs are provided above"
